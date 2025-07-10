16576b942b1a8a5ab364e4f582a36a91c45c3a53
Bagusarya <aryabagus453@gmail.com>
2025-07-10 14:09:28 +0700

    update layout

--- src/component/shared/carousel.jsx
+++ src/component/shared/carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Carousel({ children: slides, autoSlide = false, autoSlideInterval=3000 }){
   const [current, setCurrent] = useState(0);
